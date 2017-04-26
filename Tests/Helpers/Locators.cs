13c7fae7834474d1087cd505fc5fd06794b10bff
Mary Levchuk <maria.levchuk@creuna.com>
2017-04-26 12:23:52 +0300

    Added locators for Frontpage

--- Tests/Helpers/Locators.cs
+++ Tests/Helpers/Locators.cs
@@ -8,6 +8,12 @@ namespace Tests.Helpers
 {
     public static class Locators
     {
-        public const string 
+        //Frontpage
+        public const string Logo = ".site-logo .js-go-to-frontpage";
+        public const string IntroText = ".intro-text .js-intro-text .finished";
+        public const string NavigationLinks = ".site-navigation__link";
+        public const string StartBtn = ".athletes-overview__start-button .js-start";
+        public const string StartBtnText = ".athletes-overview__start-button-text .js-clickhold-text";
+
     }
 }
